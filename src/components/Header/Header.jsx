0fa9b15fc59a7c4fbaecde97bb247373209d7b4d
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2024-02-29 11:08:58 +0100

    add tailwind css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <h2 className='text-indigo-500 mt-2 uppercase'>Header</h2>
+    </div>
+  );
 }
