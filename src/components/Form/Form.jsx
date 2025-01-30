3659a009b4bf58cff8ec167ab4d3a062d1489876
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2025-01-30 21:18:31 +0100

    first comit

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,32 @@
 import React, { useState } from 'react';
 
 export default function Form() {
-  const [nombre, setNombre] = useState('Hola');
+  const [nombre, setNombre] = useState('');
+  const [propietario, setPropietario] = useState('');
+  const [email, setEmail] = useState('');
+  const [fecha, setFecha] = useState('');
+  const [sintomas, setSintomas] = useState('');
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //VALIDACIÓN DE FORMULARIO
+    // si ALGUNO DE LOS CAMPOS DEL FORMULARIO ESTAN VACÍOS, ENTONCES SETERROR PASA A TRUE, QUE HACE QUE HACE QUE APAREZCA UN P
+    // DONDE DICE TODOS LOS CAMPOS SON OBLIGATORIOS
+
+    if ([nombre, propietario, email, fecha, sintomas].includes('')) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+    // if ([nombre, propietario, email, fecha, sintomas].includes('')) {
+    //   setError(true);
+    //   return;
+    // }
+    // setError(false);
+  };
+
   return (
     <div className='md:w-1/2 lg:w-2/5'>
       <h2 className='font-black text-3xl text-center'>Seguimiento Pacientes</h2>
@@ -9,7 +34,18 @@ export default function Form() {
         Añade Pacientes y {''}
         <span className='text-indigo-600 font-bold'>Administralos</span>
       </p>
-      <form className='bg-white shadow-md rounded-lg py-10 px-5'>
+      <form
+        onSubmit={handleSubmit}
+        className='bg-white shadow-md rounded-lg py-10 px-5'
+      >
+        {/* SI ERROR ES TRUE, ENTONCES IMPRIME EL <P></P> */}
+        {error && (
+          <div>
+            <p className='bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md'>
+              Todos los campos son obligatorios
+            </p>
+          </div>
+        )}
         <div className='mb-5'>
           <label
             htmlFor='mascota'
@@ -19,6 +55,7 @@ export default function Form() {
           </label>
           <input
             value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
             id='mascota'
             type='text'
             placeholder='Nombre de la Mascota'
@@ -33,6 +70,8 @@ export default function Form() {
             Nombre Propietario
           </label>
           <input
+            value={propietario}
+            onChange={(e) => setPropietario(e.target.value)}
             id='propietario'
             type='text'
             placeholder='Nombre Propietario'
@@ -47,6 +86,8 @@ export default function Form() {
             Email
           </label>
           <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             id='email'
             type='email'
             placeholder='Email contacto'
@@ -61,6 +102,8 @@ export default function Form() {
             Fecha Alta
           </label>
           <input
+            value={fecha}
+            onChange={(e) => setFecha(e.target.value)}
             id='alta'
             type='date'
             className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
@@ -71,9 +114,11 @@ export default function Form() {
             htmlFor='sintomas'
             className='block text-gray-700 uppercase font-bold'
           >
-            Fecha Alta
+            Síntomas
           </label>
           <textarea
+            value={sintomas}
+            onChange={(e) => setSintomas(e.target.value)}
             id='sintomas'
             placeholder='Describe los síntomas'
             className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
